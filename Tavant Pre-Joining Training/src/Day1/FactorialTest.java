957a269421ce3c1915cb66e9cc27497d3f711605
MageshUdayakumar <52830190+MageshUdayakumar@users.noreply.github.com>
2019-09-05 18:42:17 +0530

    Project

--- Tavant Pre-Joining Training/src/Day1/FactorialTest.java
+++ Tavant Pre-Joining Training/src/Day1/FactorialTest.java
@@ -12,7 +12,7 @@ class FactTest {
 		int out = fact.CalculateFactorial(5);
 		assertEquals(120,out);
 		int out1 = fact.CalculateFactorial(4);
-		assertEquals(22,out);
+		assertEquals(22,out1);
 		
 	}
 
