c664ae8b1a008de74cfcaaff085bc10a26568d12
MageshUdayakumar <52830190+MageshUdayakumar@users.noreply.github.com>
2019-09-05 19:24:42 +0530

    Project

--- Tavant Pre-Joining Training/src/Day1/SumOfNumbersTest.java
+++ Tavant Pre-Joining Training/src/Day1/SumOfNumbersTest.java
@@ -8,12 +8,21 @@ class SumOfNumbersTest {
 
 	@Test
 	void test() {
+		
 		SumOfNumbers sum =new SumOfNumbers();
+		/*success*/
 		int out = sum.SumOfEvenNumber(1, 10);
 		assertEquals(30,out);
 		
 		int out1 = sum.SumOfOddNumber(1, 10);
 		assertEquals(25,out1);
+
+		/*fail*/
+		int output = sum.SumOfEvenNumber(5, 10);
+		assertEquals(21,output);
+		
+		int output1 = sum.SumOfOddNumber(5, 10);
+		assertEquals(24,output1);
 	}
 
 }
