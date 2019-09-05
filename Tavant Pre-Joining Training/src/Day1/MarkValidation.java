971c379268c3e28ae3d37fbf5fb45c127ca48368
MageshUdayakumar <52830190+MageshUdayakumar@users.noreply.github.com>
2019-09-05 18:58:50 +0530

    Project

--- Tavant Pre-Joining Training/src/Day1/MarkValidation.java
+++ Tavant Pre-Joining Training/src/Day1/MarkValidation.java
@@ -1,38 +1,40 @@
-package Day1;
+package day1;
 
 import java.util.Scanner;
 
 public class MarkValidation {
 
-	public static String isPass(int m)
+	public  String isPass(int mark)
 	{
 		
-		if(m>=40) 
+		if(mark>=40) 
 		return "Pass";
 		else
 		return "Fail";
 		
 	}
-	public static String markGrade(int m)
+	public String markGrade(int mark)
 	{
-		if(m>=90)
+		if(mark>=90)
 		return "Grade A";
-		else if(m>=75&&m<90)
+		else if(mark>=75&&mark<90)
 		return "Grade B";
-		else if(m>=60&&m<75)
+		else if(mark>=60&&mark<75)
 	    return "Grade C";
 		else
 		return  "Grade D";	
 	}
 	public static void main(String[] args) {
 
-	Scanner sc = new Scanner(System.in);
-      int m;
+      Scanner scanner = new Scanner(System.in);
+      int mark;
       System.out.println("Mark=");
-      m=sc.nextInt();
-      System.out.println(isPass(m));
-      System.out.println(markGrade(m));
-      sc.close();
+      mark=scanner.nextInt();
+      MarkValidation markvalidation = new MarkValidation();
+      System.out.println(markvalidation.isPass(mark));
+      System.out.println(markvalidation.markGrade(mark));
+      scanner.close();
 	} 
 
 }
+
