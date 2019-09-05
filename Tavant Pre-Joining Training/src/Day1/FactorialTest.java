a2d8119954b74e6130bf94f03adaaf1a009ec183
MageshUdayakumar <52830190+MageshUdayakumar@users.noreply.github.com>
2019-09-05 19:32:04 +0530

    Project

--- Tavant Pre-Joining Training/src/Day1/FactorialTest.java
+++ Tavant Pre-Joining Training/src/Day1/FactorialTest.java
@@ -4,14 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class FactTest {
+class FactorialTest {
 
 	@Test
 	void test() {
-		Factorial fact =new Factorial();
-		int out = fact.CalculateFactorial(5);
+		Factorial factorial =new Factorial();
+		
+		/*success*/
+		int out = factorial.CalculateFactorial(5);
 		assertEquals(120,out);
-		int out1 = fact.CalculateFactorial(4);
+		
+		/*fail*/
+		int out1 = factorial.CalculateFactorial(4);
 		assertEquals(22,out1);
 		
 	}
