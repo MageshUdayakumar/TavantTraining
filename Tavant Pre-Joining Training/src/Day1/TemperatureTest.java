2471b3e2655fbfd690517a1614dc4ba156c9bb34
MageshUdayakumar <52830190+MageshUdayakumar@users.noreply.github.com>
2019-09-05 19:44:27 +0530

    Project

--- Tavant Pre-Joining Training/src/Day1/TemperatureTest.java
+++ Tavant Pre-Joining Training/src/Day1/TemperatureTest.java
@@ -1,4 +1,5 @@
-package Day1;
+package day1;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -7,12 +8,19 @@ class TemperatureTest {
 
 	@Test
 	void test() {
-		Temperature temp = new  Temperature();
-		double out=temp.convertToFarenheit(0);
+		Temperature temperature = new  Temperature();
+		
+		double out=temperature.convertToFarenheit(0);
 		assertEquals(32,out);
 		
-		double out1 = temp.convertToCelsius(32);
+		double out1 = temperature.convertToCelsius(32);
 		assertEquals(0,out1);
+		
+		double output=temperature.convertToFarenheit(10);
+		assertEquals(40,output);
+		
+		double output1 = temperature.convertToCelsius(9);
+		assertEquals(30,output1);
 	}
 
 }
